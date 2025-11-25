9fc27a87e01d5fbd1ca26a2c8bf36cba985fb404
darkmoor7 <139566008+darkmoor7@users.noreply.github.com>
2025-11-25 17:18:01 -0500

    Mission terminal implementation for direction and mission level choice

--- MMOCoreORB/src/server/zone/objects/tangible/terminal/mission/MissionTerminalImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/terminal/mission/MissionTerminalImplementation.cpp
@@ -12,6 +12,8 @@
 #include "server/zone/managers/city/CityManager.h"
 #include "server/zone/managers/city/CityRemoveAmenityTask.h"
 #include "server/zone/objects/player/sessions/SlicingSession.h"
+#include "server/zone/managers/director/DirectorManager.h"
+#include "server/zone/objects/player/PlayerObject.h"
 
 void MissionTerminalImplementation::fillObjectMenuResponse(ObjectMenuResponse* menuResponse, CreatureObject* player) {
 	TerminalImplementation::fillObjectMenuResponse(menuResponse, player);
@@ -27,7 +29,22 @@ void MissionTerminalImplementation::fillObjectMenuResponse(ObjectMenuResponse* m
 		menuResponse->addRadialMenuItemToRadialID(73, 75, 3, "@city/city:east"); // East
 		menuResponse->addRadialMenuItemToRadialID(73, 76, 3, "@city/city:south"); // South
 		menuResponse->addRadialMenuItemToRadialID(73, 77, 3, "@city/city:west"); // West
-	}
+	    }
+
+	    if (terminalType == "general" || terminalType == "imperial" || terminalType == "rebel") {
+ 
+	            Lua* lua = DirectorManager::instance()->getLuaInstance();
+			    if (lua == nullptr) {
+				        return;
+			    }
+
+	    menuResponse->addRadialMenuItem(112, 3, "Choose Mission Level");
+
+	    if (!player->isGrouped()) {
+		        menuResponse->addRadialMenuItem(113, 3, "Choose Mission Direction");
+	    
+	    }
+    }
 }
 
 int MissionTerminalImplementation::handleObjectMenuSelect(CreatureObject* player, byte selectedID) {
@@ -61,25 +78,56 @@ int MissionTerminalImplementation::handleObjectMenuSelect(CreatureObject* player
 
 		return 0;
 
-	} else if (selectedID == 72) {
+	    } else if (selectedID == 72) {
 
-		if (city != nullptr && city->isMayor(player->getObjectID())) {
-			CityRemoveAmenityTask* task = new CityRemoveAmenityTask(_this.getReferenceUnsafeStaticCast(), city);
-			task->execute();
+		        if (city != nullptr && city->isMayor(player->getObjectID())) {
+			            CityRemoveAmenityTask* task = new CityRemoveAmenityTask(_this.getReferenceUnsafeStaticCast(), city);
+			            task->execute();
 
-			player->sendSystemMessage("@city/city:mt_removed"); // The object has been removed from the city.
-		}
+			            player->sendSystemMessage("@city/city:mt_removed"); // The object has been removed from the city.
+		        }
 
-		return 0;
+		         return 0;
 
-	} else if (selectedID == 74 || selectedID == 75 || selectedID == 76 || selectedID == 77) {
+	    } else if (selectedID == 74 || selectedID == 75 || selectedID == 76 || selectedID == 77) {
 
-		CityManager* cityManager = getZoneServer()->getCityManager();
-		cityManager->alignAmenity(city, player, _this.getReferenceUnsafeStaticCast(), selectedID - 74);
+		        CityManager* cityManager = getZoneServer()->getCityManager();
+		        cityManager->alignAmenity(city, player, _this.getReferenceUnsafeStaticCast(), selectedID - 74);
 
-		return 0;
-	}
+		        return 0;
+	    } else if (selectedID == 112) {
 
+		        Lua* lua = DirectorManager::instance()->getLuaInstance();
+		        if (lua == nullptr) { 
+			            return 0;
+	            }
+
+	            Reference<LuaFunction*> mission_level_choice = lua->createFunction("mission_level_choice", "openWindow", 0);
+	            if (mission_level_choice == nullptr) {
+		                return 0;
+	            }
+
+	            *mission_level_choice<< player; 
+
+	            mission_level_choice->callFunction();
+	            return 0;
+		} else if (selectedID == 113) {
+			   
+			   Lua* lua = DirectorManager::instance()->getLuaInstance();
+			   if (lua == nullptr) {
+				       return 0;
+			   }
+
+			   Reference<LuaFunction*> mission_direction_choice = lua->createFunction("mission_direction_choice", "openWindow", 0);
+			   if (mission_direction_choice == nullptr) {
+				       return 0;
+			   }
+			   *mission_direction_choice << player; 
+
+			   mission_direction_choice->callFunction();
+			   return 0;
+			}
+ 
 	return TangibleObjectImplementation::handleObjectMenuSelect(player, selectedID);
 }
 
